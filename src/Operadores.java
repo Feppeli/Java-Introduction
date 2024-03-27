7eb6439f303dadb45a8c6cce06f6a31eb907cddd
Felipe Teixeira <dfsilvat@gmail.com>
2024-03-27 15:17:00 -0300

    add: operadores

---
+++ src/Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {
+    public static void main(String[] args){
+        // Operadores
+          // Soma
+        int valor1 = 10;
+        int valor2 = 10;
+
+        int soma = valor1 + valor2;
+        System.out.println(soma);
+
+          // Subtração
+        int valor3 = 12;
+        int valor4 = 5;
+
+        int subtracao = valor3 - valor4;
+        System.out.println(subtracao);
+
+          // Divisão
+        int divisor = 2;
+        int dividendo = 60;
+
+        int divisao = dividendo / divisor;
+        System.out.println(divisao);
+
+          // Multiplicação
+        int valor5 = 3;
+        int valor6 = 33;
+
+        int multiplicacao = valor5 * valor6;
+        System.out.println(multiplicacao);
+
+    }
+}
---
+++ src/Variáveis.java
@@ -0,0 +1,16 @@
+public class Variáveis {
+    public static void main(String[] args){
+        String nome = "Douglas Felipe Teixeira";
+        byte idade = 21;
+        int numero_de_RG = 100000072;
+        double altura = 1.80;
+        float saldo_bancario = 3.58f;
+        boolean maior_de_idade = true;
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(numero_de_RG);
+        System.out.println(altura);
+        System.out.println(saldo_bancario);
+        System.out.println(maior_de_idade);
+    }
+}
